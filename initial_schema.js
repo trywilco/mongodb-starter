e20e4f38ffeb505858d59253d3f61c36917c8e36
Shem <smagnezi8@gmail.com>
2025-06-04 17:29:30 +0300

    Create initial_schema.js

---
+++ initial_schema.js
@@ -0,0 +1,9 @@
+// This is the initial schema template that needs to be completed
+const initial_schema = {
+  // TODO: Add the $jsonSchema operator here
+  bsonType: "object",
+  required: [], // TODO: Add review_id as required
+  properties: {
+    // TODO: Add review_id field with string type
+  },
+};
